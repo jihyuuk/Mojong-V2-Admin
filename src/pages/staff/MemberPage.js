69da8ee5075d1173497e495bf0aa2b622e71a396
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-17 15:55:41 +0900

    Feat : 직원 관리(차단, 승인) 기능 구현 완료!

---
+++ src/pages/staff/MemberPage.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import SubHeader from '../../components/SubHeader';
+import MemeberView from '../../components/MemberView';
+import ApprovalView from '../../components/ApprovalView';
+import axios from 'axios';
+import axiosWithToken from '../../utils/axiosWithToken';
+
+function MemberPage() {
+
+    //카테고리 : 직원목록 or 가입대기 선택여부
+    const [selected, setSelected] = useState('members');
+    const [members, setMembers] = useState([]);
+
+    //직원 목록 불러오기
+    //각 뷰로 넘기기
+    //각각 로직후 업데이트하기
+
+    useEffect(() => {
+        //맴버 불러오기
+        axiosWithToken.get('/members')
+            .then((response) => {
+                setMembers(response.data);
+            })
+            .catch((error) => {
+                alert("정보 불러오기 실패")
+            });
+
+    }, []);
+
+
+
+    return (
+        <div className='d-flex flex-column h-100  bg-white'>
+
+            {/* 헤더 */}
+            <SubHeader title='직원관리' />
+
+            {/* 카테고리 */}
+            <div className='d-flex bg-white fs-5 fw-medium text-secondary border-bottom'>
+                <div className={`text-center py-2 w-100 ${selected === 'members' ? 'selected' : ''}`} onClick={() => setSelected('members')}>직원목록</div>
+                <div className='border-end' />
+                <div className={`text-center py-2 w-100 ${selected === 'approvals' ? 'selected' : ''}`} onClick={() => setSelected('approvals')}>가입대기</div>
+            </div>
+
+            {/* 뷰 */}
+            <div className='my-content bg-white'>
+                {selected === "members" && <MemeberView members={members} setMembers={setMembers} />}
+                {selected === "approvals" && <ApprovalView members={members} setMembers={setMembers} />}
+            </div>
+
+        </div>
+    );
+}
+
+export default MemberPage;
