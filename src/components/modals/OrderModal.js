8da13b755853d7e785034489b196a1f2e73a7c68
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-19 06:50:42 +0900

    Style : 주문확인 모달 디자인 마이너 수정

--- src/components/modals/OrderModal.js
+++ src/components/modals/OrderModal.js
@@ -59,35 +59,40 @@ function OrderModal({ showModal, setShowModal, finalAmount, payment, discountAmo
                         <path d="M3 4.5a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 1 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5M11.5 4a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z" />
                         <path d="M2.354.646a.5.5 0 0 0-.801.13l-.5 1A.5.5 0 0 0 1 2v13H.5a.5.5 0 0 0 0 1h15a.5.5 0 0 0 0-1H15V2a.5.5 0 0 0-.053-.224l-.5-1a.5.5 0 0 0-.8-.13L13 1.293l-.646-.647a.5.5 0 0 0-.708 0L11 1.293l-.646-.647a.5.5 0 0 0-.708 0L9 1.293 8.354.646a.5.5 0 0 0-.708 0L7 1.293 6.354.646a.5.5 0 0 0-.708 0L5 1.293 4.354.646a.5.5 0 0 0-.708 0L3 1.293zm-.217 1.198.51.51a.5.5 0 0 0 .707 0L4 1.707l.646.647a.5.5 0 0 0 .708 0L6 1.707l.646.647a.5.5 0 0 0 .708 0L8 1.707l.646.647a.5.5 0 0 0 .708 0L10 1.707l.646.647a.5.5 0 0 0 .708 0L12 1.707l.646.647a.5.5 0 0 0 .708 0l.509-.51.137.274V15H2V2.118z" />
                     </svg>
-                    <div className='ms-2 fw-medium'>주문확인</div>
+                    <div className='ms-2 fw-medium'>주문을 확인해주세요</div>
                 </ModalTitle>
             </ModalHeader>
 
             <ModalBody className='fs-4'>
-                <div className="mb-1">
-                    <div className="text-secondary mb-3" style={{ fontSize: '1rem' }}>
-                        <span >품목 {cartItems.length}</span>
-                        <span > · 총 수량 {totalQuantity}</span>
+
+                <div className="text-secondary mb-3" style={{ fontSize: '1rem' }}>
+                    <span >품목 {cartItems.length}</span>
+                    <span > · 총 수량 {totalQuantity}</span>
+                </div>
+
+                <div className="border px-2 py-3 rounded-3 bg-body-tertiary">
+                    <div className="mb-1">
+                        <div className="text-muted small mb-1">주문 항목</div>
+                        <div className="fs-5 fw-bold ps-1">{getCartSummary()}</div>
                     </div>
-                    <div className="text-muted small mb-1">주문 항목</div>
-                    <div className="fs-5 fw-bold ps-1">{getCartSummary()}</div>
-                </div>
 
-                <div className="mb-3">
-
-                </div>
-
-
-                {discountAmount > 0 &&
                     <div className="mb-3">
-                        <div className="text-muted small mb-1">할인 금액</div>
-                        <div className="ps-1 fs-4 fw-bold text-danger">-{discountAmount.toLocaleString()}원</div>
+                        <div className="text-muted small mb-1">결제 방식</div>
+                        <div className="ps-1 fw-semibold fs-5">{payment === "CASH" ? "현금 결제 💵" : "카드 결제 💳"}</div>
                     </div>
-                }
 
-                <div className="mb-3">
-                    <div className="text-muted small mb-1">최종 결제 금액</div>
-                    <div className="ps-1 fs-4 fw-bold text-success">{finalAmount.toLocaleString()}원</div>
+
+                    {discountAmount > 0 &&
+                        <div className="mb-3">
+                            <div className="text-muted small mb-1">할인 금액</div>
+                            <div className="ps-1 fs-4 fw-semibold text-danger">-{discountAmount.toLocaleString()}원</div>
+                        </div>
+                    }
+
+                    <div className="mb-3">
+                        <div className="text-muted small mb-1">최종 결제 금액</div>
+                        <div className="ps-1 fs-4 fw-semibold text-success">{finalAmount.toLocaleString()}원</div>
+                    </div>
                 </div>
 
                 <div className="d-flex justify-content-center mt-4 py-2">
@@ -100,6 +105,7 @@ function OrderModal({ showModal, setShowModal, finalAmount, payment, discountAmo
                         onChange={(e) => setSkipReceipt(e.target.checked)}
                     />
                 </div>
+
             </ModalBody>
 
             <div className='d-flex gap-1 p-2 border-top'>
