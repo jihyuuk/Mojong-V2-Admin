f4fb5c224bbbcb8a6eaf891823867fe9dbfae4f7
jihyuuk_home <jihyuk.dev@gmail.com>
2025-04-25 11:42:31 +0900

    Style : 서브헤더 position-relative => d-flex로 변경

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -6,18 +6,20 @@ function SubHeader({ title }) {
     const navigate = useNavigate();
 
     return (
-        <header className='p-2 border-2 border-bottom border-success-subtle shadow-sm position-relative'>
+        <header className='p-2 border-2 border-bottom border-success-subtle shadow-sm d-flex align-items-center justify-content-between'>
 
             {/* 뒤로가기 버튼 */}
-            <div className='p-1' onClick={() => navigate(-1)}>
+            <div className='p-1' onClick={() => navigate(-1)} style={{ cursor: 'pointer' }}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
                     <path fillRule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
                 </svg>
             </div>
 
-            <div className='fw-semibold fs-4 position-absolute top-50 start-50 translate-middle text-center w-100' style={{ maxWidth: '80vw' }}>
+            <div className='fw-semibold fs-4 text-center'>
                 {title}
             </div>
+
+            <div style={{ width: "33px" }} />
 
         </header>
     )
